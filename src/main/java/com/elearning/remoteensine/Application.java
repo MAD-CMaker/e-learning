a3431b0aac59a19d65782b44b8e04454a98215b2
Magda Quinn <makiaquint@gmail.com>
2025-06-03 14:54:02 -0300

    Tentando Resolver conexão com o banco

--- src/main/java/com/elearning/remoteensine/Application.java
+++ src/main/java/com/elearning/remoteensine/Application.java
@@ -3,12 +3,21 @@ package com.elearning.remoteensine;
 import com.elearning.remoteensine.util.DatabaseConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-    System.out.println("\nSua aplicação de Ensino Remoto está rodando!");
-//    System.out.println("Acesse em seu navegador: http://localhost:8080");
+
+    ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+    DatabaseConnector connector = context.getBean(DatabaseConnector.class);
+    try (Connection conn = connector.getConnection()) {
+      System.out.println("✅ Teste direto no main: conexão bem-sucedida!");
+    } catch (SQLException e) {
+      System.err.println("❌ Erro ao conectar no main: " + e.getMessage());
+    }
   }
 }
